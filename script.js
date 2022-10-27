887ac0b2956d790d51b46d152fe85221a6ca94a1
Ishi Inanis <ishi.inanis@gmail.com>
2022-10-27 07:09:12 +0000

    add comments with types and params

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+/**
+ * @typedef {object} Task
+ * @param {number} id
+ * @param {string} title
+ */
 class Task {
+  /** @type {Task[]} */
   static collection = []
 
+  /** @type {string} */
   #title = ''
-
-  element = document.createElement('article')
+  /** @type {number} */
+  #id
+  /** @type {HTMLElement} */
+  #element = document.createElement('article')
 
   static load() {
+    /** @type {string[]} */
     let keys = Object.keys(localStorage),
+        /** @type {*[]} */
         tasks = [];
 
     keys = keys.filter(el => el.startsWith('task-'));
@@ -22,7 +33,12 @@ class Task {
     Task.collection.sort((current, next) => current.id > next.id ? 1 : -1);
   }
 
+  /**
+   * @returns {number}
+   * @private
+   */
   static _searchFreeID() {
+    /** @type {number} */
     let minFreeID = 1;
 
     for (let i = 0; i < Task.collection.length; i++) {
@@ -33,6 +49,7 @@ class Task {
   }
 
   save() {
+    /** @type {{id: number, title: string}} */
     const task = {
       'id': this.id,
       'title': this.title
@@ -41,25 +58,62 @@ class Task {
     localStorage.setItem(`task-${this.id}`, JSON.stringify(task));
   }
 
-  constructor(id) {
-    this.id = id;
-
-    this.element.classList.add('task');
-    this.element.id = `task-${this.id}`;
-    this.element.innerHTML = `
-      <header class="task-title"><h3></h3></header>
-      <main></main>
-      <footer class="action-buttons"><button class="delete">X</button><button class="change">I</button><button class="ok">L</button></footer>
+  /**
+   * @param {Task} task
+   * @protected
+   */
+  static #show(task) {
+    task.#element.classList.add('task');
+    task.#element.id = `task-${this.id}`;
+    task.#element.innerHTML = `
+      <header class="task-title">
+        <h3></h3>
+      </header>
+      <p></p>
+      <footer class="action-buttons">
+        <button class="delete">X</button>
+        <button class="change">I</button>
+        <button class="ok">L</button>
+      </footer>
     `;
 
-    document.getElementById('task-list').appendChild(this.element);
+    document.getElementById('task-list').appendChild(task.#element);
+  }
+
+  set id(value) {
+    if (typeof value === 'number') {
+      this.#id = value;
+    } else throw Error('"id" must be a number');
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  /** @param {number} id */
+  constructor(id) {
+    this.id = id;
+    Task.#show(this);
   }
 }
 
+/**
+ * @typedef {object} Theme
+ * @param {string} selection-background-color,
+ * @param {string} selection-text-color,
+ * @param {string} button-hover-color,
+ * @param {string} background-color,
+ * @param {string} border-color,
+ * @param {string} text-color
+ */
 class Theme {
+  // TODO: maybe protected
+  /** @type {Theme[]} */
   static collection = []
 
+  /** @param {string} color */
   static change(color = localStorage.getItem('theme')) {
+    /** @type {HTMLElement} */
     const root = document.querySelector(':root');
 
     if (typeof localStorage.theme !== 'string') {
@@ -73,6 +127,15 @@ class Theme {
     localStorage.setItem('theme', color);
   }
 
+  /**
+   * @param {string} name
+   * @param {string} selectionBackgroundColor
+   * @param {string} selectionTextColor
+   * @param {string} buttonHoverColor
+   * @param {string} backgroundColor
+   * @param {string} borderColor
+   * @param {string} textColor
+   */
   static add(
       name,
       selectionBackgroundColor,
@@ -92,6 +155,14 @@ class Theme {
     );
   }
 
+  /**
+   * @param {string} selectionBackgroundColor
+   * @param {string} selectionTextColor
+   * @param {string} buttonHoverColor
+   * @param {string} backgroundColor
+   * @param {string} borderColor
+   * @param {string} textColor
+   */
   constructor(
       selectionBackgroundColor,
       selectionTextColor,
@@ -109,10 +180,17 @@ class Theme {
   }
 }
 
-
+/**
+ * @param {string} dataKey
+ * @returns {object}
+ */
 const getLocalStorageDataJSON = (dataKey) => {
       return JSON.parse(localStorage.getItem(dataKey));
     },
+    /**
+     * @param {string} dataKey
+     * @param {object} data
+     */
     setLocalStorageDataJSON = (dataKey, data) => {
       localStorage.setItem(dataKey, JSON.stringify(data));
     };
@@ -152,9 +230,11 @@ Task.load();
 
 Theme.change();
 
-document.getElementById('new-task').onclick = () => {
-  const freeID = Task._searchFreeID(),
-      element = document.getElementById(`task-${freeID}`);
 
-  Task.collection.push(new Task(freeID));
-}
+
+// document.getElementById('new-task').onclick = () => {
+//   const freeID = Task._searchFreeID(),
+//       element = document.getElementById(`task-${freeID}`);
+//
+//   Task.collection.push(new Task(freeID));
+// }
